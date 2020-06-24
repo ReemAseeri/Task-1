d465c11417126c4ae12f76cb8d4ed61eda5ffe61
ReemAseeri <67390109+ReemAseeri@users.noreply.github.com>
2020-06-24 22:18:54 +0300

    Add files via upload

---
+++ Task_1_Code.ino
@@ -0,0 +1,20 @@
+void setup()
+{
+ 
+ 
+  
+  pinMode(6, OUTPUT);
+  pinMode(5, OUTPUT);
+  pinMode(4, OUTPUT);
+  pinMode(3, OUTPUT);
+}
+
+void loop()
+{
+  digitalWrite(6,HIGH);
+  digitalWrite(4,HIGH);
+  delay(1000);
+  
+  
+  
+}
